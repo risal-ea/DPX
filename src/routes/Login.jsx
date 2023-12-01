3db2004fb71b3bbe9c27001242adf405cbe74f03
risal_ea <risalea.22@gmail.com>
2023-12-01 14:49:34 +0530

    bug fix

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,24 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
 import { FaFacebookF, FaGoogle, FaTwitter } from "react-icons/fa";
 import { googleProvider, auth } from "../firebase";
 import { signInWithPopup } from "firebase/auth";
 
 const Login = () => {
-  //LOGIN WITH GOOGLE
-    const loginWithGoogle = async () => {
-        try {
-        await signInWithPopup(auth, googleProvider);
-        } catch (err) {
-        console.error("Error with Auth With Google:", err);
-        }
-    };
+  // LOGIN WITH GOOGLE
+  const loginWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+    } catch (error) {
+      console.error("Error signing in with Google:", error.message);
+    }
+  };
 
   return (
-    <div >
-        <h1>Login</h1>
-        <a onClick={loginWithGoogle} className="social">
-            <FaGoogle />
-        </a>
+    <div>
+      <h1>Login</h1>
+      <button onClick={loginWithGoogle} className="social-button">
+        <FaGoogle />
+        Login with Google
+      </button>
     </div>
   );
 };
