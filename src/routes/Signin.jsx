9647871b474ea3f6d917a8d58c8a39d7e602806d
risal_ea <risalea.22@gmail.com>
2023-12-06 19:47:04 +0530

    improving user experiance

--- src/routes/Signin.jsx
+++ src/routes/Signin.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import { motion as m } from "framer-motion";
-import { Link, useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form";
-import { AiOutlineMail, AiFillLock } from "react-icons/ai";
-import { emailValidate, passwordValidate } from "../utils/form-validate";
+import { useNavigate } from "react-router-dom";
 import { useGoogleAuth, useLogin } from "../hooks/auth";
+import { FaGoogle } from "react-icons/fa";
 import "../global.css";
 
 const Signin = () => {
@@ -23,61 +20,30 @@ const Signin = () => {
   };
 
   return (
-    <m.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ delay: 1, duration: 1 }}
-      className="w-full max-w-[350px] h-screen m-auto p-4 flex flex-col justify-center bg-white text-black"
-    >
-      {/* Logo */}
-      <Link
-        to={'/home'}
-        title="Home"
-        className="group text-5xl font-bold mb-12 text-center tracking-widest"
-      >
-        <span className="group-hover:text-orange-600 duration-150 ease-in">
-          C
-        </span>
-        <span className="group-hover:text-orange-600 duration-150 delay-100 ease-in">
-          L
-        </span>
-        <span className="group-hover:text-orange-600 duration-150 delay-200 ease-in">
-          O
-        </span>
-        <span className="group-hover:text-orange-600 duration-150 delay-[300ms] ease-in">
-          S
-        </span>
-        <span className="group-hover:text-orange-600 duration-150 delay-[400ms] ease-in">
-          B
-        </span>
-        <span className="group-hover:text-orange-600 duration-150 delay-[500ms] ease-in">
-          E
-        </span>
-      </Link>
-
-      {/* Title */}
-      <h1
-        className={`${
-          error ? "text-[#BF0000]" : "text-black"
-        } text-left tracking-wider pt-8 pb-2`}
-      >
-        Join now and stay connected with friends!
-      </h1>
-
+    <div className="signin-page">
       
+      <h1 className="label">Glad You Here!</h1>
+      <p className="sub-label">Let’s close with the World</p>
 
-        {/* Submit Button */}
-        <button onClick={handleGoogleLogin} className="w-full my-2 p-3 bg-white hover:bg-orange-600 border-2 border-black hover:border-orange-600 text-black hover:text-white tracking-wider rounded-2xl shadow-none hover:shadow-md shadow-orange-600 font-bold duration-300 ease-in-out">
-          {isLoading ? (
-            <span className="animate-pulse">Loading...</span>
-          ) : (
-            <di className="google-auth-btn">
-              <p className="google-auth-btn-txt">Continue with Google</p>
-              <img width="8%" src="google.svg"/>
-            </di>
-          )}
-        </button>
-    </m.div>
+      <img className="network-img" src="network.svg"/>
+
+      {/* Submit Button */}
+      <button
+        onClick={handleGoogleLogin}
+        className="google-auth-btn"
+        disabled={isLoading}
+      >
+        {isLoading ? (
+          <span>Loading...</span>
+        ) : (
+          <>
+            <p className="google-auth-btn-txt">Continue with Google</p>
+            <FaGoogle className="google-auth-icon" />
+          </>
+        )}
+      </button>
+      <p className="btn-sub-label">Explore now on closbe!</p>
+    </div>
   );
 };
 
